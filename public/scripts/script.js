c9ce8305ce5195fc0ff407eed3acb95b1b80482d
Craig <craigjw87@gmail.com>
2016-06-20 08:53:26 -0500

    first commit

---
+++ public/scripts/script.js
@@ -0,0 +1,98 @@
+$(document).ready( function(){
+  // console.log("hi from script.js");
+  domUpdate();
+
+
+//--------------------------------------------------------------------------------
+
+  $( "#submitTask").on( "click", function(){
+    // console.log("hi from the submit button");
+
+    var newTask = $( "#taskInput" ).val().toUpperCase();
+
+    // console.log( newTask );
+
+// Object....ASSEMBLE!!!!--------------------------------------------------------------------------------
+    var newTaskObject = {
+      "task": newTask,
+      "completed": false
+    };
+
+// AJAX Post-------------------------------------------------------------------------------------------------------
+  $.ajax( {
+    type: "POST",
+    url: "/createTask",
+    data: newTaskObject,
+    success: function(){
+      domUpdate();
+      // console.log("in /postRoute success");
+      },
+      error: function(err, exc){
+      alert( "POST error: " + err.status );
+      }
+    }); // End of ajax call
+    $('#taskInput').val(''); // Clears input field after submit
+  }); // End submit button
+
+  // Delete Button --------------------------------------------------------------------------------
+
+      $( "#outputDiv").on( "click", "#deleteButton", function(){
+        console.log("hi from delete button");
+
+        var taskId = {
+            'id': $(this).attr('taskId')
+          };
+          // console.log('this id completed: ' + $(this).attr('taskId'));
+          $(this).parent().remove();
+        $.ajax( {
+          type: "POST",
+          url: "/deleteTask",
+          data: taskId,
+          success: function(){
+            // console.log("in /deleteButton success");
+            }
+          }); // End of ajax call
+      }); // End of Delete button function
+
+// Checkbox --------------------------------------------------------------------------------
+
+    $( "#outputDiv").on( "click", "#taskCheck", function(){
+      // console.log("hi from taskCheck yo");
+      // console.log('this id completed: ' + $(this).attr('taskId'));
+      var taskId = {
+          'id': $(this).attr('taskId')
+        };
+
+      $.ajax( {
+        type: "POST",
+        url: "/updateTask",
+        data: taskId,
+        success: function(){
+          }
+        }); // End of ajax call
+  }); // End of checkbox function
+
+}); // End Doc.ready jquery
+
+// AJAX Get-------------------------------------------------------------------------------------------------------
+var domUpdate = function(){
+  $('#outputDiv').empty();
+  $.ajax( {
+    type: "GET",
+    url: "/getTask",
+    success: function( data ){
+      // console.log("in /getTask success");
+      showTask ( data );
+      } // End success
+    }); // End ajax
+  }; // End of domUpdate function
+
+  function showTask ( allTasks ){
+  // console.log( 'in showTask:' + allTasks );
+  for( i=0; i<allTasks.length; i++ ){
+    var taskOut = "<p>" + allTasks[i].task + "  Completed: " + allTasks[i].completed + "</p>";
+        // $( '#outputDiv' ).append( taskOut );
+        $( '.outputDiv ' ).prepend( '<li class="active" id="taskDiv"><input type="checkbox" id="taskCheck" taskId="' + allTasks[i].id + '">'  + allTasks[i].task + '<button type="button" id="deleteButton" taskId="' + allTasks[i].id + '">Delete Task</button></li>' );
+        // console.log( "New Task: " + allTasks[ i ].id );
+      } // End of loop
+    } // End of showTask function
---
+++ server/app.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+var pg = require("pg");
+var connectionString = 'postgres://localhost:5432/to_do';
+
+
+// Setting up static page ------------------------------------------------------------------------------------------------------
+app.use ( express.static( "public" ));
+
+// Route to index.html (resovled path to html)-------------------------------------------------------------------------------------------------------
+app.get ("/", function( req, res ){
+  res.sendFile( path.resolve("views/index.html"));
+});
+
+// New Task POST -------------------------------------------------------------------------------------------------------
+app.post ( "/createTask", urlencodedParser, function ( req, res ){
+  console.log( req.body );
+// Send new task to data base
+  pg.connect( connectionString, function( err, client, done ){
+    var query =  client.query ( "INSERT INTO todolist ( task, completed ) VALUES ( $1, $2 )", [ req.body.task, req.body.completed ] );
+      done();
+      query.on('end', function(){
+        console.log("POST END");
+      return res.end();
+    }); // End query.on
+  }); // End of pg
+}); // End of post
+
+// Delete POST -------------------------------------------------------------------------------------------------------
+app.post ( "/deleteTask", urlencodedParser, function ( req, res ){
+// Send new task to data base
+  pg.connect( connectionString, function( err, client, done ){
+    console.log(req.body.id);
+    var query =  client.query ( 'DELETE from todolist where id=' +req.body.id+ ';' );
+    done();
+      query.on('end', function(){
+      return res.end();
+    }); // End query.on
+  }); // End of pg
+}); // End of post
+
+// Delete POST -------------------------------------------------------------------------------------------------------
+app.post ( "/updateTask", urlencodedParser, function ( req, res ){
+// Send new task to data base
+  pg.connect( connectionString, function( err, client, done ){
+    console.log(req.body.id);
+    var query =  client.query ( 'UPDATE todolist set completed=true where id=' +req.body.id+ ';' );
+    done();
+    res.end();
+  }); // End of pg
+}); // End of post
+
+// GET -------------------------------------------------------------------------------------------------------
+
+app.get ( "/getTask", urlencodedParser, function ( req, res ){
+  console.log( "Hello from getTask app.get" );
+
+  var allTasks = [];
+
+  pg.connect ( connectionString, function ( err, client, done ){
+    var query = client.query ( "SELECT * FROM todolist");
+    done();
+    var rows = 0;
+    query.on( 'row', function ( row ){
+    allTasks.push( row );
+      });
+    query.on( 'end', function (){
+    return res.json( allTasks );
+    });
+  }); // End pg.connect function
+}); // End of app.get
+
+// On the server-------------------------------------------------------------------------------------------------------
+app.listen ( 3000, "localhost", function( req, res ){
+  // console.log( "listening from server 3000" );
+});
